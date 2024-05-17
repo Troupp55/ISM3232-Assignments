46386cba2a9b0235b6cb659c51653aa6b7b07abf
Tristen Roupp <tristenr@usf.edu>
2024-05-17 14:43:02 -0600

    Create and test calcTip function.

--- CodingChallenge2/cc_1.js
+++ CodingChallenge2/cc_1.js
@@ -4,3 +4,13 @@ var totalCost;
 
 tip = (billAmount > 50 && billAmount < 300) ? 0.15 : 0.2;
 console.log("Tip percent is: ", tip);
+
+
+function calcTip(billAmount){
+    tipPercent = (billAmount > 50 && billAmount < 300) ? 0.15 : 0.2;
+    var tipAmount = billAmount * tipPercent; // Corrected this line
+    return tipAmount;
+}
+
+const testTip = calcTip(billAmount);
+console.log("Tip amount is: $", testTip);
