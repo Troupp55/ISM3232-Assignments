7e34a13d85f774e0f075abdf18bb3f7febb25904
Tristen Roupp <tristenr@usf.edu>
2024-05-17 15:59:41 -0600

    Implement and populate arrays for bills, tips, and totals.

--- CodingChallenge2/cc_1.js
+++ CodingChallenge2/cc_1.js
@@ -19,3 +19,35 @@ console.log("Tip amount is: $", testTip);
 totalCost = testTip + billAmount;
 console.log("The bill amount was, ", billAmount, "the tip was, $", testTip, "The total cost was, $",totalCost);
 
+//commit 4
+const billsSet1 = [275, 40, 430];
+const billsSet2 = [125, 555, 44];
+
+const tips= [];
+
+billsSet1.forEach(function(i){
+    const tipAmount = calcTip(i);
+    tips.push(tipAmount);
+});
+
+console.log("Data set 2");
+billsSet2.forEach(function(i){
+    const tipAmount = calcTip(i);
+    tips.push(tipAmount);
+});
+
+const totalsBillSet1 = [];
+billsSet1.forEach(function(billAmount){
+    const tipAmount = calcTip(billAmount);
+    totalsBillSet1.push(billAmount + tipAmount);
+});
+
+const totalsBillSet2 = [];
+billsSet2.forEach(function(billAmount){
+    const tipAmount = calcTip(billAmount);
+    totalsBillSet2.push(billAmount + tipAmount);
+});
+
+console.log(totalsBillSet1);
+console.log(totalsBillSet1);
+console.log(tips);
