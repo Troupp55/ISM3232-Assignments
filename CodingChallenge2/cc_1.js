937f14911aefc8d6ef15ef3311b7724ea81b904c
Tristen Roupp <tristenr@usf.edu>
2024-05-17 15:01:04 -0600

    Add output functionality to display bill, tip, and total.

--- CodingChallenge2/cc_1.js
+++ CodingChallenge2/cc_1.js
@@ -1,11 +1,11 @@
 var tip;
-var billAmount = 100;
+var billAmount = 275;
 var totalCost;
 
 tip = (billAmount > 50 && billAmount < 300) ? 0.15 : 0.2;
 console.log("Tip percent is: ", tip);
 
-
+//commit 3
 function calcTip(billAmount){
     tipPercent = (billAmount > 50 && billAmount < 300) ? 0.15 : 0.2;
     var tipAmount = billAmount * tipPercent; // Corrected this line
@@ -14,3 +14,8 @@ function calcTip(billAmount){
 
 const testTip = calcTip(billAmount);
 console.log("Tip amount is: $", testTip);
+
+//commit 2
+totalCost = testTip + billAmount;
+console.log("The bill amount was, ", billAmount, "the tip was, $", testTip, "The total cost was, $",totalCost);
+
