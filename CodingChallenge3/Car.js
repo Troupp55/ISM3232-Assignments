d5027d32c13a300f777a7cf661d07ad57816c589
Tristen Roupp <tristenr@usf.edu>
2024-05-21 15:28:52 -0600

    Add brake method to decrease speed by 5 km/h

--- CodingChallenge3/Car.js
+++ CodingChallenge3/Car.js
@@ -11,9 +11,17 @@ class Car{
         console.log("The cars speed in km/h is: ", speed);
         return speed;
     }   
+//Add brake method to decrease speed by 5 km/h.
+    brake(speed) {
+        speed = speed - 5;
+        console.log("The cars new speed is: ",speed)
+        return speed;
+    }
 }
 //Add accelerate method to increase speed by 10 km/h
 const car1 = new Car("Toyota", 100);
 const updatedSpeed = car1.accelerate(car1.speed);
 console.log(car1.speed);
 console.log(car1.make)
+
+const car2 =new Car()
