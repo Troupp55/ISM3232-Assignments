8fd948c687f987b569e5a733e180c9ad38f432bb
Tristen Roupp <tristenr@usf.edu>
2024-05-21 15:19:01 -0600

    Add accelerate method to increase speed by 10 km/h

--- CodingChallenge3/Car.js
+++ CodingChallenge3/Car.js
@@ -4,6 +4,16 @@ class Car{
     constructor(make, speed){
         this.make = make;
         this.speed = speed;
-    }
-
+    } 
+    
+    accelerate(speed) {
+        speed = speed + 10;
+        console.log("The cars speed in km/h is: ", speed);
+        return speed;
+    }   
 }
+//Add accelerate method to increase speed by 10 km/h
+const car1 = new Car("Toyota", 100);
+const updatedSpeed = car1.accelerate(car1.speed);
+console.log(car1.speed);
+console.log(car1.make)
